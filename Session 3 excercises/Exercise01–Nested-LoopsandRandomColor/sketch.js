89fee961eaa7d237494fd561fdf5eb1f3d9b37a2
ellie270898 <ellieharrodine@gmail.com>
2017-11-25 14:50:07 +0000

    p5

--- Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
+++ Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
@@ -15,4 +15,10 @@ for (let y=0; y <10; y++) {
     rect(size * x, size * y, size, size);
   }
  }
+
+ for (var i = 0; i < 100; i++) {
+  var r = random(50);
+  stroke(r*5);
+  line(50, i, 50+r, i);
+}
 }
