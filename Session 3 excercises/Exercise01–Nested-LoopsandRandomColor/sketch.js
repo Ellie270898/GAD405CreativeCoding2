ed7beb0e6c83282ce2921425f1928f6877093d91
ellie270898 <ellieharrodine@gmail.com>
2017-11-25 14:38:42 +0000

    grid drawn

---
+++ Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
@@ -0,0 +1,22 @@
+let size = 50;
+
+function setup() {
+  createCanvas (500, 500);
+  noLoop();
+}
+
+function draw() {
+  fill(215, 200, 170);
+
+rect(0, height/2, size, size);
+rect(50, height/2, size, size);
+rect(100, height/2, size, size);
+rect(150, height/2, size, size);
+rect(200, height/2, size, size);
+rect(250, height/2, size, size);
+rect(300, height/2, size, size);
+rect(350, height/2, size, size);
+rect(400, height/2, size, size);
+rect(450, height/2, size, size);
+
+}
