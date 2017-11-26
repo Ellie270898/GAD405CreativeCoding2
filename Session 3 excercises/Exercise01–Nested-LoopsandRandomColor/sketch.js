7c481b1c9a70954d1ed9ee1068f218236450f669
ellie270898 <ellieharrodine@gmail.com>
2017-11-26 14:29:06 +0000

    quad

--- Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
+++ Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
@@ -13,8 +13,14 @@ for (let y=0; y <10; y++) {
   for (let x = 0; x < 10; x++) {
     //Draw the shape at the corresponding postion
     rect(size * x, size * y, size, size);
+   }
   }
- }
+
+  quad(38, 31, 86, 20, 69, 63, 30, 76);
+  
+  translate(width/2, height/2);
+  rotate(PI/3.0);
+  rect(-26, -26, 52, 52);
 
  for (var i = 0; i < 100; i++) {
   var r = random(-50, 50);
