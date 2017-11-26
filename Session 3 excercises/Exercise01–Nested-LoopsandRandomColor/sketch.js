feeb7791baa079f93e52302ac6255af2938f6f3a
ellie270898 <ellieharrodine@gmail.com>
2017-11-26 14:51:20 +0000

    second fix

--- Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
+++ Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
@@ -1,17 +1,38 @@
 let size = 50;
 
-function setup() {
+function setup()
+{
   createCanvas (500, 500);
   noLoop();
 }
 
-function draw() {
+function draw()
+{
   fill(215, 200, 170);
 
 //run the nested loop for x and y
-for (let y=0; y <10; y++) {
-  for (let x = 0; x < 10; x++) {
+  for (let y=0; y <10; y++)
+  {
+    for (let x = 0; x < 10; x++)
+    {
     //Draw the shape at the corresponding postion
-    rect(size * x, size * y, size, size);
-   }
+      rect(size * x, size * y, size, size);
+    }
   }
+}
+
+function draw()
+{
+    quad(38, 31, 86, 20, 69, 63, 30, 76);
+
+
+    translate(width/2, height/2);
+    rotate(PI/3.0);
+    rect(-26, -26, 52, 52);
+
+    for (var i = 0; i < 100; i++)
+    {
+      var r = random(-50, 50);
+      line(50,i,50+r,i);
+    }
+}
