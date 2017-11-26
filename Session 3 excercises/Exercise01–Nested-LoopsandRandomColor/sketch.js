1d2bcaad73c2b25d0a3be282ad65f746f76c363a
ellie270898 <ellieharrodine@gmail.com>
2017-11-26 14:52:19 +0000

    third change

--- Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
+++ Session 3 excercises/Exercise01–Nested-LoopsandRandomColor/sketch.js
@@ -20,19 +20,3 @@ function draw()
     }
   }
 }
-
-function draw()
-{
-    quad(38, 31, 86, 20, 69, 63, 30, 76);
-
-
-    translate(width/2, height/2);
-    rotate(PI/3.0);
-    rect(-26, -26, 52, 52);
-
-    for (var i = 0; i < 100; i++)
-    {
-      var r = random(-50, 50);
-      line(50,i,50+r,i);
-    }
-}
