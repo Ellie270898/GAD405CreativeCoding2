284ad75cdac821813cd4e7b1dac02b722552ed0f
ellie270898 <ellieharrodine@gmail.com>
2017-12-04 16:59:31 +0000

    circular motion first attempt

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -1,4 +1,6 @@
 let Opacityshape;
+let angle = 0;
+
 function setup() {
 var canvas = createCanvas(594, 841);
 //background(0,0,0);
@@ -14,7 +16,7 @@ background(0,0,0);
 //translate(random(width), random(height));
 // star();
 //pop();
-noLoop();
+//noLoop();
 
 //create 100 stars in random positions on the canvas
 for(let i=0; i<1000; i++){
@@ -23,10 +25,20 @@ translate(random(width), random(height));
 star(i);
 pop();
 
+//This code should create the circular motion for the galaxy.
+angle += 0.1;
+let sinValueX = sin(angle);
+let sinValueY = cos(angle)*0.7;
+
+let x = map(sinValueX, -1, 1, 0, width-50);
+let y = map(sinValueY, -1, 1, 0, width-50);
+
+fill(255);
+ellipse(25+x, 25+y, 50, 50);
+noLoop();
  }
 }
-
-
+//This is the code for a singular star. The opacity is random, the colour is white and the size is 3 pixels.
 function star(){
   Opacityshape=random(255)
   fill(255,255,255,Opacityshape);
