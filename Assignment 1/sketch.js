70ecf1638e2c20f1ac55cab5a5f1278ba5b2d6c9
ellie270898 <ellieharrodine@gmail.com>
2017-12-04 15:39:13 +0000

    edit to code, and formatting in CSS

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -1,6 +1,8 @@
-function Setup() {
-createCanvas(594, 841);
+function setup() {
+var canvas = createCanvas(594, 841);
 //background(0,0,0);
+canvas.parent("myContainer");
+canvas.class("myCanvas");
 }
 
 function draw(){
@@ -12,18 +14,22 @@ background(0,0,0);
 //translate(random(width), random(height));
 // star();
 //pop();
-//noLoop();
-}
+noLoop();
 
-for(let i=0; i<100; i++){
+//create 100 stars in random positions on the canvas
+for(let i=0; i<400; i++){
 push();
 translate(random(width), random(height));
 star(i);
 pop();
+
+}
 }
 
 
 function star(){
   fill(255,255,255);
+  //Opacity(random(255));
 ellipse(5, 5, 5, 5);
+
 }
