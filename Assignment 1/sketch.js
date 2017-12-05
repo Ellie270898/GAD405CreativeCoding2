37f56b00631b1ef3346e154ded1bceb481b6f698
ellie270898 <ellieharrodine@gmail.com>
2017-12-05 16:31:08 +0000

    galaxy loop is sort of fixed

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -9,7 +9,7 @@ canvas.class("myCanvas");
 }
 
 function draw(){
-background(0,0,0);
+//background(0,0,0);
 //!!FAILED CODE!!
 //star();
 //push();
@@ -19,30 +19,38 @@ background(0,0,0);
 //noLoop();
 
 //create 100 stars in random positions on the canvas
-for(let i=0; i<1000; i++){
-push();
-translate(random(width), random(height));
-star(i);
-pop();
+for(let i=0; i < 1000; i++)
+{
+  push();
+  translate(random(width), random(height));
+  star(i);
+  pop();
+  //noLoop();
+}
+
+//This code is for the stars and planets that fill the galaxy.
+ellipse(250, 250, 6, 6);
+ellipse(253, 260, 6, 6);
+ellipse(146, 300, 6, 6);
+ellipse(400, 300, 8, 8);
 
 //This code should create the circular motion for the galaxy.
-angle += 0.1;
-let sinValueX = sin(angle);
-let sinValueY = cos(angle)*0.7;
 
-let x = map(sinValueX, -1, 1, 0, width-50);
-let y = map(sinValueY, -1, 1, 0, width-50);
+  angle += 0.1;
+  let sinValueX = sin(angle);
+  let sinValueY = cos(angle)*0.5;
 
-fill(255);
-ellipse(25+x, 25+y, 50, 50);
-noLoop();
- }
-}
+  let x = map(sinValueX, -1, 1, 0, width-50);
+  let y = map(sinValueY, -1, 1, 0, width-50);
+
+ fill(255);
+ ellipse(25+x, 25+y, 50, 50);
+
 //This is the code for a singular star. The opacity is random, the colour is white and the size is 3 pixels.
 function star(){
   Opacityshape=random(255)
   fill(255,255,255,Opacityshape);
   //Opacity(random());
   ellipse(3,3,3,3);
-
+  }
 }
